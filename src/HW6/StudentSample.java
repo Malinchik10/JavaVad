95eab6caeb709eafc69bb3a701ab68601d826b96
Malinchik10 <ketty.malinamonaco@gmail.com>
2024-10-01 22:32:34 +0300

    Правки класса студент

--- src/HW6/StudentSample.java
+++ src/HW6/StudentSample.java
@@ -1,10 +1,11 @@
 package HW6;
 
+import java.util.Calendar;
 import java.util.Date;
 
 public class StudentSample {
     public static void main(String[] args) {
-        Student student = new Student(1, "Михаил", "Овечкин", new Date(1988, 01,01), "Инновационное развитие", 2, "ИТ");
+        Student student = new Student(1, "Михаил", "Овечкин", new Date(1988, Calendar.DECEMBER,01), "Инновационное развитие", 2, "ИТ");
         student.information();
         System.out.println("--------------------------------------------------");
         var lastName = student.getLastName();
